c847bb252f4873b07a42c2dbd44c940ed2ccc9af
Pedro Henrique <78853588+pedrohemmel@users.noreply.github.com>
2023-05-29 16:31:15 -0300

    feat: Creation of tabbar and your items

--- MacroChallenge/Controller/TabBarViewController.swift
+++ MacroChallenge/Controller/TabBarViewController.swift
@@ -1,25 +1,47 @@
 //
-//  TabBarViewController.swift
+//  TabBaraViewController.swift
 //  MacroChallenge
 //
 //  Created by Pedro henrique Dias hemmel de oliveira souza on 29/05/23.
 //
 
-import Foundation
 import UIKit
 
-class TabBarViewController: UIViewController {
-    
-    lazy var text: UILabel = {
-        let text = UILabel()
-        text.text = "Pedrolas"
-        text.textColor = .black
-        return text
-    }()
-    
+class TabBarViewController: UITabBarController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        self.view.addSubview(self.text)
+        self.setupViewConfiguration()
     }
 }
+
+//MARK: - ViewCode
+extension TabBarViewController: ViewCode {
+    func buildViewHierarchy() {
+    }
+    
+    func setupConstraints() {
+    }
+    
+    func setupAdditionalConfiguration() {
+        self.tabBar.backgroundColor = .white
+        self.tabBar.isTranslucent = false
+        self.setupTabItems()
+    }
+}
+
+//MARK: - Functions here
+extension TabBarViewController {
+    private func setupTabItems() {
+        let exampleViewController = UINavigationController(rootViewController: ExampleViewController())
+        let exampleSecondaryViewController = UINavigationController(rootViewController: ExampleSecondaryViewController())
+        
+        self.setViewControllers([exampleViewController, exampleSecondaryViewController], animated: true)
+        
+        guard let items = self.tabBar.items else { return }
+        
+        items[0].image = UIImage(systemName: "function")
+        items[1].image = UIImage(systemName: "person.circle")
+    }
+}
+
